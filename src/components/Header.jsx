53fa2139bc890afc08856783de8f93740dc2b5ce
Farhan Shakir <89987458+farhanshakir@users.noreply.github.com>
2024-08-23 18:57:17 +0500

    Update Header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,24 +13,24 @@ export default function Header() {
   return (
     <div className="headerContainer">
       <div className="headerTextContainer">
-        <h3>Sr. UI/UX Designer | Google UX Design Pro Certified</h3>
+        <h3>Product Designer | Google UX Design Pro Certified</h3>
         <h1>I’M Farhan S.</h1>
         <h4>
-          Sr. UI/UX Designer <br /> Google UX Design Pro Certified
+          Product Designer <br /> Google UX Design Pro Certified
         </h4>
         <div className="headerBtncontainerMob">
           <button type="button" className="headerLocationBtn">
-            <PlaceIcon sx={{ color: '#0E88FA' }} /> Lahore, Pakistan
+            <PlaceIcon sx={{ color: '#0E88FA' }} /> Abu Dhabi, UAE
           </button>
           <a href="mailto:farhan11.fk22@gmail.com" className="headerEmailBtn">
-            <EmailIcon /> farhan11.fk22@gmail.com
+            <EmailIcon /> Contact Me!
           </a>
         </div>
         <p>
-          Hello! I'm an accomplished UI/UX Designer, dedicated to creating exceptional user
-          experiences. With a programming background & 5 years of experience in the field, I bring a
+          Hello! I'm an accomplished Product Designer, dedicated to creating exceptional user
+          experiences. With a programming background & over 6 years of experience in the field, I bring a
           unique blend of technical expertise and design acumen. My dedication to understanding user
-          needs and translating them into visually captivating interfaces has led to successful 40+
+          needs and translating them into visually captivating interfaces has led to successful 60+
           projects across various industries.
           <br />
           <br />
@@ -40,10 +40,10 @@ export default function Header() {
         </p>
         <div className="headerBtncontainer">
           <button type="button" className="headerLocationBtn">
-            <PlaceIcon sx={{ color: '#0E88FA' }} /> Lahore, Pakistan
+            <PlaceIcon sx={{ color: '#0E88FA' }} /> Abu Dhabi, UAE
           </button>
           <a href="mailto:farhan11.fk22@gmail.com" className="headerEmailBtn">
-            <EmailIcon /> farhan11.fk22@gmail.com
+            <EmailIcon /> Contact Me!
           </a>
         </div>
       </div>
